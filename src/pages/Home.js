669b79fddda72bd82a0e50fbb9a6bdaeed05a8cc
Burnsje2024S <burnsje20@gmail.com>
2025-04-28 22:06:25 -0400

    upd 1
    all my changes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,65 +1,68 @@
 import React from 'react';
-import {Route, Routes, Link, BrowserRouter} from "react-router-dom";
-import {Blog} from './Blog'
-import {About} from './About'
-import {Land} from "./Land";
+import { Robots } from "./Robots";
+import { Route, Routes, Link, BrowserRouter } from "react-router-dom";
+import { Blog } from './Blog'
+import { About } from './About'
+import { Land } from "./Land";
 import data from './navigation.json'
 import './Home.css'
 import themes from './themes.json'
-import {Team} from "./Team";
-import {WhatWeDo} from "./WhatWeDo";
+import { Team } from "./Team";
+import { WhatWeDo } from "./WhatWeDo";
 
 const theme = window.matchMedia('{prefers-color-scheme: dark}').matches;
 function setTheme(themestr) { //TODO: Switch to enum
-		const theme = themes[themestr];
-		const keys = Object.keys(theme);
-		const style = document.body.style;
-		keys.map(
-				(key) => {
-						style.setProperty('--' + key, theme[key])
-				}
-		)
-		style.background = theme["bg"];
+	const theme = themes[themestr];
+	const keys = Object.keys(theme);
+	const style = document.body.style;
+	keys.map(
+		(key) => {
+			style.setProperty('--' + key, theme[key])
+		}
+	)
+	style.background = theme["bg"];
 }
 
 function Header() {
-		setTheme('light')
-		return (
-				<div id="header">
-						<img src='icons/menu.svg' id="hamburber">
-						</img>
-						<Link to="/" id="logo_container"><img src='images/DART.svg' id="logo">
-						</img></Link>
-						<nav id="navPanel">
-								{
-										Object.keys(data).map((key, index) => (
-												<li key={index} className="navItem">
-														<Link to={data[key]}>
-																{key}
-														</Link>
-												</li>
-										))
-								}
-						</nav>
-				</div>
-		)
+	setTheme('light')
+	return (
+		<div id="header">
+			<img src='icons/menu.svg' id="hamburber">
+			</img>
+			<Link to="/" id="logo_container"><img src='images/DART.svg' id="logo">
+			</img></Link>
+			<nav id="navPanel">
+				{
+					Object.keys(data).map((key, index) => (
+						<li key={index} className="navItem">
+							<Link to={data[key]}>
+								{key}
+							</Link>
+						</li>
+					))
+				}
+			</nav>
+		</div>
+	)
 }
 
 function Home() {
-		return (
-				<BrowserRouter>
-						<Header />
-						<div id='content'>
-								<Routes>
-										<Route index element={<Land/>}/>
-										<Route path="/blog" element={<Blog/>}/>
-										<Route path="/about" element={<About/>}/>
-										<Route path="/team" element={<Team/>}/>
-										<Route path="/what-we-do" element={<WhatWeDo/>}/>
-								</Routes>
-						</div>
-				</BrowserRouter>
-		)
+	return (
+		<BrowserRouter>
+			<Header />
+			<div id='content'>
+				<Routes>
+					<Route index element={<Land />} />
+					<Route path="/blog" element={<Blog />} />
+					<Route path="/about" element={<About />} />
+					<Route path="/team" element={<Team />} />
+					<Route path="/what-we-do" element={<WhatWeDo />} />
+					<Route path="/robots" element={<Robots />} />
+				</Routes>
+
+			</div>
+		</BrowserRouter>
+	)
 }
 
 export default Home;
---
+++ src/pages/Robots.js
@@ -0,0 +1,48 @@
+import React from "react";
+import "./Robots.css";
+import { Link } from "react-router-dom";
+
+const robotList = [
+  {
+    name: "Storm Surge",
+    type: "Spinner Bot",
+    image: "/images/stormsurge.png",
+    description: "High-speed spinner designed for aggressive takedowns in VEX-style combat.",
+    link: "/robots/storm-surge"
+  },
+  {
+    name: "Pinhead Larry",
+    type: "Flipper Bot",
+    image: "/images/pinhead.png",
+    description: "Robust flipper robot engineered for precision control and endurance battles.",
+    link: "/robots/pinhead-larry"
+  },
+  {
+    name: "Eggbeater",
+    type: "Vertical Spinner",
+    image: "/images/eggbeater.png",
+    description: "Compact yet powerful vertical spinner ideal for maneuverability and knockout hits.",
+    link: "/robots/eggbeater"
+  },
+  
+];
+
+export function Robots() {
+  return (
+    <div className="robots-page">
+      <h1 className="robots-title">Our Robots</h1>
+      <div className="robots-grid">
+        {robotList.map((robot, index) => (
+          <Link to={robot.link} className="robot-card" key={index}>
+            <img src={robot.image} alt={robot.name} className="robot-image" />
+            <div className="robot-info">
+              <h3>{robot.name}</h3>
+              <p><em>{robot.type}</em></p>
+              <p>{robot.description}</p>
+            </div>
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
+}
